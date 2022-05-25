f65d20e60cab5fca933e88590272baf11d871d77
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-05-25 09:42:36 -0300

    Concluido requisito 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ProductsList from './components/ProductsList';
+import ShoppingCart from './pages/ShoppingCart';
 // import './App.css';
 
 function App() {
   return (
     <main>
-      <p>Iahuuuu!</p>
       <Router>
+        <Link to="/shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
         <Switch>
           <Route exact path="/" component={ ProductsList } />
+          <Route path="/shoppingCart" component={ ShoppingCart } />
         </Switch>
       </Router>
     </main>
