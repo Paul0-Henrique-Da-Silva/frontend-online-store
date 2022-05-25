4a36bbdc39ed5a2f7f7b9d1bb7d66fd157007f26
petrucci934 <petrucci.lucenaplc@gmail.com>
2022-05-25 16:19:50 -0300

    Feito desafio 05

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,42 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class ProductsList extends React.Component {
   state = {
     categorias: [],
+    inputSearch: '',
+    recebeProdutos: [],
   }
 
-  async componentDidMount() {
-    const categorias = await getCategories();
-    this.setState({
-      categorias,
-    });
+  componentDidMount() {
+    this.buscaCategorias();
   }
 
-  render() {
-    const { categorias } = this.state;
-    return (
-      <div>
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-        <Link to="/shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
-        <div>
-          <p>Categorias:</p>
-          {
-            categorias.map((categoria) => (
-              <label data-testid="category" key={ categoria.id } htmlFor={ categoria.id }>
-                { categoria.name }
-                <input type="radio" name={ categoria.name } id={ categoria.id } />
-              </label>
-            ))
-          }
+ handleChange = ({ target }) => {
+   const { name, value } = target;
+   this.setState({ [name]: value });
+ }
 
-        </div>
-      </div>
-    );
-  }
+ buscaCategorias = async () => {
+   const categorias = await getCategories();
+   this.setState({
+     categorias,
+   });
+ };
+
+ handleClick = async () => {
+   const { inputSearch } = this.state;
+   const resultApi = await getProductsFromCategoryAndQuery(null, inputSearch);
+   console.log(resultApi);
+   this.setState({ recebeProdutos: resultApi.results });
+ }
+
+ render() {
+   const { categorias, inputSearch, recebeProdutos } = this.state;
+   return (
+     <div>
+       <h1 data-testid="home-initial-message">
+         Digite algum termo de pesquisa ou escolha uma categoria.
+       </h1>
+       <Link to="/shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
+       <div>
+         <p>Categorias:</p>
+         {
+           categorias.map((categoria) => (
+             <label data-testid="category" key={ categoria.id } htmlFor={ categoria.id }>
+               { categoria.name }
+               <input
+                 type="radio"
+                 name="categoria-selecionada"
+                 id={ categoria.id }
+                 value={ categoria.name }
+                 onChange={ this.handleChange }
+               />
+             </label>
+           ))
+         }
+       </div>
+       <input
+         type="text"
+         data-testid="query-input"
+         name="inputSearch"
+         value={ inputSearch }
+         onChange={ this.handleChange }
+       />
+       <button
+         type="button"
+         data-testid="query-button"
+         onClick={ this.handleClick }
+       >
+         Buscar
+       </button>
+
+       { recebeProdutos.length === 0 ? <h1>Nenhum produto foi encontrado</h1>
+         : (
+           <div>
+             {recebeProdutos.map((produto) => (
+               <div key={ produto.id } data-testid="product">
+                 <p>{produto.title}</p>
+                 <img src={ produto.thumbnail } alt={ produto.title } />
+                 <p>{produto.price}</p>
+               </div>))}
+           </div>)}
+
+     </div>
+   );
+ }
 }
 
 export default ProductsList;
