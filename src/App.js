88f43136098b38cef07727514d9eb5947f2967fa
petrucci934 <petrucci.lucenaplc@gmail.com>
2022-05-25 12:03:39 -0300

    Feitas alterações em requisito 04

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProductsList from './components/ProductsList';
 import ShoppingCart from './pages/ShoppingCart';
-import { getCategories } from './services/api';
+
 // import './App.css';
 
 class App extends React.Component {
-state = {
-  categorias: [],
-}
+  render() {
+    return (
+      <main>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ ProductsList } />
+            <Route path="/shoppingCart" component={ ShoppingCart } />
+          </Switch>
+        </Router>
 
-async componentDidMount() {
-  const categorias = await getCategories();
-  this.setState({
-    categorias,
-  });
-}
-
-render() {
-  const { categorias } = this.state;
-  return (
-    <main>
-      <Router>
-        <Link to="/shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
-        <Switch>
-          <Route exact path="/" component={ ProductsList } />
-          <Route path="/shoppingCart" component={ ShoppingCart } />
-        </Switch>
-      </Router>
-      <div>
-        <p>Categorias:</p>
-        {
-          categorias.map((categoria) => (
-            <label data-testid="category" key={ categoria.id } htmlFor={ categoria.id }>
-              { categoria.name }
-              <input type="radio" name={ categoria.name } id={ categoria.id } />
-            </label>
-          ))
-        }
-
-      </div>
-    </main>
-  );
-}
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class ProductsList extends React.Component {
+  state = {
+    categorias: [],
+  }
+
+  async componentDidMount() {
+    const categorias = await getCategories();
+    this.setState({
+      categorias,
+    });
+  }
+
   render() {
+    const { categorias } = this.state;
     return (
-      <h1 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h1>
+      <div>
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+        <Link to="/shoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
+        <div>
+          <p>Categorias:</p>
+          {
+            categorias.map((categoria) => (
+              <label data-testid="category" key={ categoria.id } htmlFor={ categoria.id }>
+                { categoria.name }
+                <input type="radio" name={ categoria.name } id={ categoria.id } />
+              </label>
+            ))
+          }
+
+        </div>
+      </div>
     );
   }
 }
